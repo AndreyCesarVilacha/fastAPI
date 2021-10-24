b71eaa79686bb3f6baeff1b2642a26889934a40e
Andrey Cesar <bleach138@gmail.com>
2021-10-24 13:48:50 -0300

    adicionando comentarios

--- books.py
+++ books.py
@@ -7,6 +7,6 @@ app = FastAPI()
 #usando o decorador para dizer que o metodo get na raiz("/")
 #vai retorna essa função
 @app.get("/")
-#Criando uma função async
+#Criando uma função async que vai ser retornada quando acessar a raiz
 async def first_api():
     return {"message": "Hello Andrey Cesar"}
