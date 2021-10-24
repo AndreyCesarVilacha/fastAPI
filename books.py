9bf08e36fdd159cb5d4b44db8293e49bec3a57c6
Andrey Cesar <bleach138@gmail.com>
2021-10-24 13:44:53 -0300

    Criando a primeira resposta

--- books.py
+++ books.py
@@ -3,3 +3,10 @@ from fastapi import FastAPI
 
 #Criando uma variavel do tipo FastAPI
 app = FastAPI()
+
+#usando o decorador para dizer que o metodo get na raiz("/")
+#vai retorna essa função
+@app.get("/")
+#Criando uma função async
+async def first_api():
+    return {"message": "Hello Andrey Cesar"}
