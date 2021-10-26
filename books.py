85a797c2b73d318a8e43b3e4a2d4e65d2e9c8533
Andrey Cesar <bleach138@gmail.com>
2021-10-26 13:25:57 -0300

    Criando uma lista de opções de caminho na docu

--- books.py
+++ books.py
@@ -1,5 +1,7 @@
 #Importando o fastAPI
 from fastapi import FastAPI
+#Importando o Enum
+from enum import Enum
 
 #Criando uma variavel do tipo FastAPI
 app = FastAPI()
@@ -12,6 +14,13 @@ BOOKS ={
     'book_4': {'title': 'Title Four', 'author': 'Author Four'},
     'book_5': {'title': 'Title Five', 'author': 'Author Five'},
 }
+
+#Criando uma classe para enumerar as opções na documentação da api
+class DirectionName(str, Enum):
+    north = "North"
+    south = "South"
+    east = "East"
+    west = "West"
 
 #usando o decorador para dizer que o metodo get na raiz("/")
 #vai retorna essa função
@@ -30,3 +39,15 @@ async def get_book_title(book_title):
 @app.get("/books/id_{book_id}")
 async def get_book_id(book_id: int):
     return {"Book id: ": book_id}
+
+#Criando a função que vai controlar a opção escolhida
+@app.get("/directions/{direction_name}")
+async def get_direction(direction_name: DirectionName):
+    if direction_name == DirectionName.north:
+        return {"Direction": direction_name, "sub": "up"}
+    if direction_name == DirectionName.south:
+        return {"Direction": direction_name, "sub": "down"}
+    if direction_name == DirectionName.east:
+        return {"Direction": direction_name, "sub": "right"}
+    if direction_name == DirectionName.west:
+        return {"Direction": direction_name, "sub": "left"}
