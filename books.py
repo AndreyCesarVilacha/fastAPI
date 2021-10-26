a1aaa85e36396decc83dc98b5b3f4271e2c26a62
Andrey Cesar <bleach138@gmail.com>
2021-10-26 12:42:06 -0300

    Criando a dicionario BOOKS

--- books.py
+++ books.py
@@ -4,6 +4,15 @@ from fastapi import FastAPI
 #Criando uma variavel do tipo FastAPI
 app = FastAPI()
 
+#Dicionario com o nome 'BOOKS'
+BOOKS ={
+    'book_1': {'title': 'Title One', 'author': 'Author One'},
+    'book_2': {'title': 'Title Two', 'author': 'Author Two'},
+    'book_3': {'title': 'Title Three', 'author': 'Author Three'},
+    'book_4': {'title': 'Title Four', 'author': 'Author Four'},
+    'book_5': {'title': 'Title Five', 'author': 'Author Five'},
+}
+
 #usando o decorador para dizer que o metodo get na raiz("/")
 #vai retorna essa função
 @app.get("/")
