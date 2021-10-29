348c901f8c5fa5830c1f7126e5de5c1ce261d28b
Andrey Cesar <bleach138@gmail.com>
2021-10-29 14:53:52 -0300

    Criando um exemplo de entrada caso para api.

--- books2.py
+++ books2.py
@@ -1,7 +1,9 @@
 from fastapi import FastAPI
 #Model para classes
 from pydantic import BaseModel, Field
+#Para gerar id unicas
 from uuid import UUID
+#Para criar campos opcionais
 from typing import Optional
 
 app = FastAPI()
@@ -17,9 +19,15 @@ BOOKS = []
 
 @app.get("/")
 async def read_all_books():
+    if leb(BOOKS) <1:
+        create_book_no_api()
     return BOOKS
 
 @app.post("/")
 async def create_book(book: Book):
     BOOKS.append(book)
     return book
+
+def create_book_no_api():
+    book_1 = Book(id="00b2d180-4f22-4e9f-bdf1-7566ad22b366", title="Titulo 1", author="Autor 1", description="Descrição 1", rating=50)
+    BOOKS.append(book_1)
