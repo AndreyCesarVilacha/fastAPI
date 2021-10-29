1aaa41beb61b6517d821a0fca1c4e0383faf1f0e
Andrey Cesar <bleach138@gmail.com>
2021-10-29 14:26:25 -0300

    adicionando regras para os atributos da classebook

--- books2.py
+++ books2.py
@@ -1,16 +1,17 @@
 from fastapi import FastAPI
 #Model para classes
-from pydantic import BaseModel
+from pydantic import BaseModel, Field
 from uuid import UUID
+from typing import Optional
 
 app = FastAPI()
 
 class Book(BaseModel):
     id: UUID
-    title: str
-    author: str
-    description: str
-    rating: int
+    title: str = Field(min_length=1)
+    author: str = Field(min_length=1, max_length=100)
+    description: Optional[str] = Field(title="Descrição do livro", max_length=100,min_length=1)
+    rating: int = Field(gt=-1, lt=101)
 
 BOOKS = []
 
